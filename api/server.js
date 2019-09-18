5c56a1c9d551fd5c7e3f4c92b1cfc95909698e60
sammy <sammy@macbook-pro-de-sammy.home>
2019-09-18 19:22:39 +0200

    API

--- api/server.js
+++ api/server.js
@@ -1,12 +1,22 @@
+
 const express = require('express'),
-    path = require('path'),
-    bodyParser = require('body-parser'),
-    cors = require('cors'),
-    mongoose = require('mongoose');
+path = require('path'),
+bodyParser = require('body-parser'),
+cors = require('cors'),
+mongoose = require('mongoose'),
+config = require('./DB');
 
-    const app = express();
-    let port = process.env.PORT || 4000;
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)}
+);
 
-    const server = app.listen(function(){
-        console.log('Listening on port ' + port);
-    });
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, function(){
+ console.log('Listening on port ' + port);
+});
