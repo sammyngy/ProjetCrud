9c716ca8e406717efd02f51865082e8cbc424c8b
sammy <sammy@macbook-pro-de-sammy.home>
2019-09-19 11:45:43 +0200

    Modif Product import

--- src/app/product-get/product-get.component.ts
+++ src/app/product-get/product-get.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import Product from '../Product';
+import Product from '../../../api/models/Product';
 import { ProductsService } from '../products.service';
 
 @Component({
